e31071516723dc6fa013658b61c0cd4c736fda2c
Nathan Duckett <nduckett13@gmail.com>
2020-06-07 22:49:19 +1200

    Clarify name changes - Add upload file to upload a piped in text document to wiki

---
+++ upload.py
@@ -0,0 +1,28 @@
+import requests
+import sys
+import yaml
+from os import path
+
+file_dir = path.dirname(path.abspath(__file__))
+
+def load_config():
+    """Load yaml config"""
+    with open(f"{file_dir}/config.yaml") as f:
+        config = yaml.load(f, Loader=yaml.SafeLoader)
+        return config
+
+def uploadContent(content):
+    requests.put(f"{config['rootURI']}/projects/{config['projectID']}/wikis/{config['wikiSlug']}",
+    headers={"PRIVATE-TOKEN": config['PAT']},
+    params={"content": content})
+
+# Global config data
+config = load_config()
+
+# Load data from STDIN piped into application
+content = ""
+for line in sys.stdin:
+    content += line
+
+# Upload to Gitlab
+uploadContent(content)
